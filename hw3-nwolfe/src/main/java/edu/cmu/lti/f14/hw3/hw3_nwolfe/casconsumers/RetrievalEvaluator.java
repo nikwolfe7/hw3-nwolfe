7d589db1560b045742f45f0d12c066179bd7baef
nikwolfe7 <nikwolfe7@gmail.com>
2014-10-21 09:18:51 -0400

    khgfhjgfj

--- hw3-nwolfe/src/main/java/edu/cmu/lti/f14/hw3/hw3_nwolfe/casconsumers/RetrievalEvaluator.java
+++ hw3-nwolfe/src/main/java/edu/cmu/lti/f14/hw3/hw3_nwolfe/casconsumers/RetrievalEvaluator.java
@@ -7,10 +7,7 @@ import java.io.PrintStream;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.HashMap;
-import java.util.Iterator;
-import java.util.List;
 import java.util.Map;
-import java.util.Vector;
 
 import org.apache.uima.UIMA_IllegalStateException;
 import org.apache.uima.cas.CAS;
