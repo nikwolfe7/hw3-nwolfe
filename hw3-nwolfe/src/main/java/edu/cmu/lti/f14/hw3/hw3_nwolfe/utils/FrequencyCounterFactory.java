da7187d846ed365c76186563f717952356e1b47a
nikwolfe7 <nikwolfe7@gmail.com>
2014-10-27 04:45:45 -0400

    sdfgsdf

--- hw3-nwolfe/src/main/java/edu/cmu/lti/f14/hw3/hw3_nwolfe/utils/FrequencyCounterFactory.java
+++ hw3-nwolfe/src/main/java/edu/cmu/lti/f14/hw3/hw3_nwolfe/utils/FrequencyCounterFactory.java
@@ -2,27 +2,33 @@ package edu.cmu.lti.f14.hw3.hw3_nwolfe.utils;
 
 public class FrequencyCounterFactory {
   private static String cleanStem = "cleanStem";
+
   private static String stemStopWord = "stemStopWord";
+
   private static String stopWord = "stopWord";
+
   private static String freq = "freq";
+
   private static String lemma = "lemma";
+
   private static String stem = "stem";
+
   private static String current = cleanStem;
-  
+
   public static FrequencyCounter getNewFrequencyCounter() {
-    if(current.equals(cleanStem)) 
+    if (current.equals(cleanStem))
       return new CleanStemCounter();
-    else if(current.equals(stemStopWord))
+    else if (current.equals(stemStopWord))
       return new CleanStemStopWordCounter();
-    else if(current.equals(stopWord))
+    else if (current.equals(stopWord))
       return new StopWordCounter();
-    else if(current.equals(freq))
+    else if (current.equals(freq))
       return new FrequencyCounter();
-    else if(current.equals(lemma))
+    else if (current.equals(lemma))
       return new LemmatizeCounter();
-    else if(current.equals(stem))
+    else if (current.equals(stem))
       return new StemCounter();
-   else
-     return new FrequencyCounter();
+    else
+      return new FrequencyCounter();
   }
 }
